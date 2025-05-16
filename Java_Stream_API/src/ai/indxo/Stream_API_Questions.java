c61e303cbb8e43b0746dccfbc5d4c4c11815e246
mr-manishx <manishkr10123@gmail.com>
2025-05-17 05:23:28 +0530

    modified on Stream API questions set

--- Java_Stream_API/src/ai/indxo/Stream_API_Questions.java
+++ Java_Stream_API/src/ai/indxo/Stream_API_Questions.java
@@ -1,8 +1,10 @@
 package ai.indxo;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 class IndxoEmployee {
@@ -25,46 +27,59 @@ class IndxoEmployee {
 		this.yearOfJoining = yearOfJoining;
 		this.salary = salary;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
 	public int getYearOfJoining() {
 		return yearOfJoining;
 	}
+
 	public void setYearOfJoining(int yearOfJoining) {
 		this.yearOfJoining = yearOfJoining;
 	}
+
 	public double getSalary() {
 		return salary;
 	}
+
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
@@ -74,8 +89,7 @@ class IndxoEmployee {
 		return "IndxoEmployee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
-	
-	
+
 }
 
 public class Stream_API_Questions {
@@ -100,31 +114,83 @@ public class Stream_API_Questions {
 		employees.add(new IndxoEmployee(13, "Mike", 29, "Male", "IT", 2019, 70000));
 		employees.add(new IndxoEmployee(14, "Nina", 28, "Female", "HR", 2020, 56000));
 		employees.add(new IndxoEmployee(15, "Oscar", 36, "Male", "Finance", 2011, 82000));
-		
+
 //		for(IndxoEmployee emp : employees) {
 //			System.out.println(emp);
 //		}
-		
+
 		// Q1. How many male and female employees are there in the organization ?
-		
-		Map<String, Long> collect = employees.stream().collect(Collectors.groupingBy(IndxoEmployee :: getGender, Collectors.counting()));
+
+		Map<String, Long> collect = employees.stream()
+				.collect(Collectors.groupingBy(IndxoEmployee::getGender, Collectors.counting()));
 
 		System.out.println(collect);
-		
-		System.out.println("---------------------");
-		
-		// Q2. Print the name of all departments in the organization ?
-		
-		employees.stream().map(IndxoEmployee :: getDepartment).distinct().forEach(dept -> System.out.println(dept));
-		
-		System.out.println("----------------------");
-		
-		// Q3. What is the average age of male and female employees ?
-		
-		Map<String, Double> collect2 = employees.stream().collect(Collectors.groupingBy(IndxoEmployee :: getGender, Collectors.averagingInt(IndxoEmployee :: getAge)));
-		System.out.println(collect2);
-		
 
+		System.out.println("---------------------");
+
+		// Q2. Print the name of all departments in the organization ?
+
+		employees.stream().map(IndxoEmployee::getDepartment).distinct().forEach(dept -> System.out.println(dept));
+
+		System.out.println("----------------------");
+
+		// Q3. What is the average age of male and female employees ?
+
+		Map<String, Double> collect2 = employees.stream().collect(
+				Collectors.groupingBy(IndxoEmployee::getGender, Collectors.averagingInt(IndxoEmployee::getAge)));
+		System.out.println(collect2);
+
+//		Q4. Get the details of highest paid employee in the organization ?
+
+		Optional<IndxoEmployee> optional = employees.stream()
+				.collect(Collectors.maxBy(Comparator.comparingDouble(IndxoEmployee::getSalary)));
+
+		if (optional.isPresent()) {
+			IndxoEmployee employee = optional.get();
+			System.out.println(employee);
+		}
+
+//		Q5. Get the names of all employees who have joined after 2015 ?
+
+		employees.stream().filter(e -> e.yearOfJoining > 2015).map(e -> e.name).forEach(name -> System.out.println(name));
+
+//		Q6. Count the number of employees in each department ?
+
+		Map<String, Long> map = employees.stream()
+				.collect(Collectors.groupingBy(IndxoEmployee::getDepartment, Collectors.counting()));
+		System.out.println(map);
+
+//		Q7. What is the average salary of each department ?
+
+		Map<String, Double> map1 = employees.stream().collect(
+				Collectors.groupingBy(IndxoEmployee::getDepartment, Collectors.averagingDouble(IndxoEmployee::getSalary)));
+		System.out.println(map1);
+
+//		Q8. Get the details of youngest male employee in the Development department ?
+
+		Optional<IndxoEmployee> optional1 = employees.stream()
+				.filter(e -> e.getGender().equals("Male") && e.getDepartment().equals("Development"))
+				.min(Comparator.comparing(IndxoEmployee::getAge));
+
+		if (optional1.isPresent()) {
+			System.out.println(optional.get());
+		}
+
+//		Q9. Who has the most working experience in the organization ?
+
+		Optional<IndxoEmployee> optional2 = employees.stream()
+				.collect(Collectors.minBy(Comparator.comparing(IndxoEmployee::getYearOfJoining)));
+
+		if (optional2.isPresent()) {
+			System.out.println(optional.get());
+		}
+
+//		Q10. How many male and female employees are there in the Sales team ?
+
+		Map<String, Long> map2 = employees.stream().filter(e -> e.getDepartment().equals("Sales"))
+				.collect(Collectors.groupingBy(IndxoEmployee::getGender, Collectors.counting()));
+
+		System.out.println(map2);
 
 	}
 
