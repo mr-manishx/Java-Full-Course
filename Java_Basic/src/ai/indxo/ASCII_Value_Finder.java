63f6955e082d4cff7a0a151411e6904fdb786dfa
mr-manishx <manishkr10123@gmail.com>
2025-08-22 13:41:27 +0530

    ASCII value finder

---
+++ Java_Basic/src/ai/indxo/ASCII_Value_Finder.java
@@ -0,0 +1,12 @@
+package ai.indxo;
+
+public class ASCII_Value_Finder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int chr = 'Z';
+		System.out.println("The ASCII value of Z is: "+chr);
+	}
+
+}
